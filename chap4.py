b0bb5c1d7a418d8555fcd1f2f2c9c4070c350695
makora <makora@hts-MacBook-Pro.local>
2014-12-08 23:59:00 +0900

    first commit

---
+++ chap4.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+#-*- encoding: utf-8 -*-
+
+"""
+
+"""
+
+import numpy
+
+
+# The NumPy ndarray: A Multidimensional Array Object
+# Slide No.5
+data = numpy.array([[0.9526, -0.246, -0.8856],
+                      [0.5639, 0.2379, 0.9104]])
+
+print data
+print data * 10
+print data + data
+
+print data.shape
+print data.dtype
+
+# Creating ndarrays
+# Slide No.6
+
+# End of Line.
